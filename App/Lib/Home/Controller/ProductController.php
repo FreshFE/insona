e6116d1aab9c4d5e975381007c7c29d2f8df21cc
MinoWu <minowu@foxmail.com>
2013-04-21 21:03:13 +0800

    合并product和product view。

--- App/Lib/Home/Controller/ProductController.php
+++ App/Lib/Home/Controller/ProductController.php
@@ -22,4 +22,33 @@ class ProductController extends Controller
 
 		$this->display();
 	}
+
+	public function view()
+	{
+		if(isset($_GET['aid']))
+		{
+			$condition['product_driver_id'] = $_GET['aid'];
+		}
+		else {
+			echo '没有输入aid, 404!';
+			exit();
+		}
+
+		$model = M('Product');
+		$condition['hidden'] = 1;
+		$page = $_GET['page'] ? $_GET['page'] : 1;
+		$listRow = 20;
+
+
+		$datas = $model->where($condition)->page($page, $listRow)->select();
+		$this->assign('datas', $datas);
+
+		$pager = $model->where($condition)->pager($page, $listRow);
+		$this->assign('pager', $pager);
+
+		$father = M('ProductCategory')->find($condition['product_driver_id']);;
+		$this->assign('father', $father);
+
+		$this->display();
+	}
 }
