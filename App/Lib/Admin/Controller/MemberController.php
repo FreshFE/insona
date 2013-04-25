bbf19d63977f1b952506f13836a0d6f1891fcfc9
MinoWu <minowu@foxmail.com>
2013-04-25 14:57:33 +0800

    更新CompanyMember管理。

---
+++ App/Lib/Admin/Controller/MemberController.php
@@ -0,0 +1,14 @@
+<?php
+
+use Smartadmin\Controller\Content as Controller;
+
+class MemberController extends Controller
+{
+	protected $model_name = 'CompanyMember';
+
+	// protected $image_thumb_name = 'thumb';
+
+	// protected $cover_thumb_name = '392x422';
+
+	// protected $category_id = 1;
+}
