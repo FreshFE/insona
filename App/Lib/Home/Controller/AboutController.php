0ad76e8b90433ad811187ba3ec34ad9f5d881e3c
MinoWu <minowu@foxmail.com>
2013-04-25 13:13:06 +0800

    更新Home的About。

--- App/Lib/Home/Controller/AboutController.php
+++ App/Lib/Home/Controller/AboutController.php
@@ -6,7 +6,14 @@ class AboutController extends Controller
 {
 	public function index()
 	{
-		$contact = M('Introduction')->find(3);
+		$Intro = M('Introduction');
+
+		// 基本介绍
+		$intro = $Intro->find(1);
+		$this->assign('intro', $intro);
+
+		// 联系信息
+		$contact = $Intro->find(3);
 		$this->assign('contact', $contact);
 
 		$this->display();
