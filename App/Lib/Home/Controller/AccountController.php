c6e23d06f9520ebf4546424742ca613fa5971b3d
MinoWu <minowu@foxmail.com>
2013-04-26 09:20:56 +0800

    整理AccountController代码和注释。

--- App/Lib/Home/Controller/AccountController.php
+++ App/Lib/Home/Controller/AccountController.php
@@ -10,14 +10,25 @@ use Think\Url as Url;
 
 use Think\Library\Upload\Upload as Upload;
 
+/**
+ * 账户管理类
+ * 控制用户注册、登录、退出等功能
+ */
 class AccountController extends Controller
 {
+	/**
+	 * 空方法名执行empty
+	 * empty分析后执行到post或get方法上
+	 */
 	public function _empty()
 	{
 		$method = (Request::is('post') ? 'post_' : 'get_') . ACTION_NAME;
 		$this->$method();
 	}
 
+	/**
+	 * 注册的get方法
+	 */
 	public function get_register()
 	{
 		$this->check_auth();
@@ -25,6 +36,9 @@ class AccountController extends Controller
 		$this->display();
 	}
 
+	/**
+	 * 注册的post方法
+	 */
 	public function post_register()
 	{
 		try {
@@ -54,10 +68,13 @@ class AccountController extends Controller
 			}
 		} catch(Exception $error)
 		{
-			Debug::output($error);
+			Redirect::error($error->getMessage());
 		}
 	}
 
+	/**
+	 * 登录的get方法
+	 */
 	public function get_login()
 	{
 		$this->check_auth();
@@ -65,6 +82,9 @@ class AccountController extends Controller
 		$this->display();
 	}
 
+	/**
+	 * 登录的post方法
+	 */
 	public function post_login()
 	{
 		$model = M('CompanyMember');
@@ -90,10 +110,13 @@ class AccountController extends Controller
 			}
 		} catch(Exception $error)
 		{
-			Debug::output($error);
+			Redirect::error($error->getMessage());
 		}
 	}
 
+	/**
+	 * 检查用户是否登录，如果已经登录无法操作
+	 */
 	protected function check_auth()
 	{
 		if(Session::get('com_auth_key'))
@@ -102,20 +125,43 @@ class AccountController extends Controller
 		}
 	}
 
+	/**
+	 * 设置来访页面的url，将该url存在session内
+	 */
 	protected function set_url_session()
 	{
-		Session::set('account_last_url', empty($_SERVER["HTTP_REFERER"]) ? __GROUP__ : $_SERVER["HTTP_REFERER"]);
+		if(strpos($_SERVER['HTTP_REFERER'], 'account') === false)
+		{
+			Session::set('account_last_url', empty($_SERVER["HTTP_REFERER"]) ? __GROUP__ : $_SERVER["HTTP_REFERER"]);
+		}
+		else {
+			Session::set('account_last_url', __GROUP__);
+		}
 	}
 
+	/**
+	 * 保存登录信息
+	 */
 	protected function set_auth($user_id)
 	{
 		Session::set('com_auth_key', $user_id);
 		Redirect::success('登录成功', Session::get('account_last_url'));
 	}
 
+	/**
+	 * 退出账户
+	 */
 	public function logout()
 	{
 		Session::set('com_auth_key', null);
 		Redirect::success('退出成功');
+	}
+
+	/**
+	 * 返回登录
+	 */
+	public function loginback()
+	{
+		Redirect::success('登录返回', Session::get('account_last_url'));
 	}
 }
