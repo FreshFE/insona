f64b6d41125e367e52ff15bd46fef18828ab417a
MinoWu <minowu@foxmail.com>
2013-04-26 22:46:20 +0800

    添加找回密码功能。

--- App/Lib/Home/Controller/AccountController.php
+++ App/Lib/Home/Controller/AccountController.php
@@ -7,6 +7,7 @@ use Think\Debug as Debug;
 use Think\Redirect as Redirect;
 use Think\Session as Session;
 use Think\Url as Url;
+use Think\Import as Import;
 
 use Think\Library\Upload\Upload as Upload;
 
@@ -114,6 +115,88 @@ class AccountController extends Controller
 		}
 	}
 
+	public function get_forget()
+	{
+		$this->display();
+	}
+
+	public function post_forget()
+	{
+		// 查找是否存在该用户
+		$data = M('CompanyMember')->where(array('email' => $_POST['email']))->find();
+
+		if($data)
+		{
+			// 存在的话，检查code是否存在近期的
+			$forget = M('Forget')->where(array('email' => $_POST['email']))->find();
+
+			if($forget)
+			{
+				$id = M('Forget')->where(array('id' => $forget['id']))->save(array('createline' => time()));
+			}
+			else {
+				$id = M('Forget')->add(array(
+					'code' => uniqid(),
+					'email' => $_POST['email'],
+					'createline' => time()
+				));
+			}
+
+			$forget = M('Forget')->find($id);
+
+			// $link
+			$link = $_SERVER['HTTP_ORIGIN'] . __URL__ . '/findpassword?code=' . $forget['code'];
+
+			// 发送Email
+			$title = "inSona 找回密码";
+			$send = '点击以下链接找回您的密码，两小时内有效<br/><a href="'.$link.'">'.$link.'</a>';
+			$tosend = $forget['email'];
+
+			$this->sendMail($title, $send, $tosend);
+
+			Redirect::success('找回邮件已经发送到您的邮箱，请及时查收，两小时内有效');
+		}
+		else {
+			// 不存在的话返回
+			Redirect::error('该邮箱地址未注册过inSona，检查一下是否拼写错误');
+		}
+	}
+
+	public function get_findpassword()
+	{
+		$this->display();
+	}
+
+	public function post_findpassword()
+	{
+		$model = M('Forget');
+		
+		if(isset($_POST['code']))
+		{
+			$code = $model->where(array('code' => $_POST['code']))->find();
+
+			if($code)
+			{
+				$data = M('CompanyMember')->where(array('email' => $code['email']))->find();
+				if($data)
+				{
+					$user_id = M('CompanyMember')->where(array('id' => $data['id']))->save(array('password' => sha1($_POST['password'])));
+					M('Forget')->where(array('code' => $_POST['code']))->delete();
+					Redirect::success('密码重置成功，请重新登录', Url::make('account/login'));
+				}
+				else {
+					Redirect::error('不存在您重置密码的账户，请重新在找回密码页面中输入您的电子邮箱');
+				}
+			}
+			else {
+				Redirect::error('您使用的重置密码页面已经超过两个小时，请重新在找回密码页面中输入您的电子邮箱');
+			}
+		}
+		else {
+			Redirect::error('您使用的重置密码页面操作无效，请重新在找回密码页面中输入您的电子邮箱');
+		}
+	}
+
 	/**
 	 * 检查用户是否登录，如果已经登录无法操作
 	 */
@@ -176,5 +259,51 @@ class AccountController extends Controller
 		else {
 			$this->json(array('success' => 0, 'error_msg' => $model->getError()));
 		}
+	}
+
+	/**
+	 * 用于发送邮件的类
+	 *
+	 * @param string $title 邮件标题
+	 * @param string $send 邮件内容
+	 * @param string $sendto 收件人的邮件地址，可以使用","来写入多个电子邮件地址
+	 */
+	protected function sendMail($title, $send, $sendto) {
+	    
+	    //标题防止乱码
+	    $title = iconv("utf-8", "gbk", $title);
+	    $title = "=?GB2312?B?" . base64_encode($title) . "?=";
+
+	    // 实例化
+	    Import::load(CORE_PATH . 'Library/Phpmailer/Phpmailer.class.php');
+	    $phpmailer = new PhpMailer();
+
+	    // 配置
+	    $phpmailer->IsSMTP();
+	    $phpmailer->SMTPAuth 	= true;
+	    $phpmailer->Host 		= 'smtp.exmail.qq.com';
+	    $phpmailer->Port 		= 25;
+	    $phpmailer->Username 	= 'service@meside.com';
+	    $phpmailer->Password 	= 'mesidewin8';
+	    $phpmailer->From 		= 'service@meside.com';
+	    $phpmailer->FromName 	= 'inSona';
+	    $phpmailer->CharSet 	= "utf-8";
+	    //$phpmailer->AddReplyTo($this->site['smtp_user'],$this->site['sitename']);
+
+	    // 遍历发件人
+	    $mails = explode(',', $sendto);
+
+	    foreach ($mails as $key => $val) {
+	        $phpmailer->AddAddress($val);
+	    }
+
+	    // 设置发送信息
+	    $phpmailer->Subject 	= $title;
+	    $phpmailer->WordWrap 	= 80;
+	    $phpmailer->MsgHTML($send);
+	    $phpmailer->IsHTML(true);
+
+	    // 发送
+	    $phpmailer->Send();
 	}
 }
