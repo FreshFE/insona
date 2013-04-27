94f7754cf7dcf4ea5fb894cee68c00f0a5b88b7f
MinoWu <minowu@foxmail.com>
2013-04-27 20:02:45 +0800

    关闭系列搜索。

--- App/Lib/Home/Controller/SearchController.php
+++ App/Lib/Home/Controller/SearchController.php
@@ -17,16 +17,16 @@ class SearchController extends Controller
 
 		// 产品
 		$product = M('Product')->where($condition)->select();
-		$driver = M('Driver')->where($condition)->select();
-		$third = M('Third')->where($condition)->select();
+		// $driver = M('Driver')->where($condition)->select();
+		// $third = M('Third')->where($condition)->select();
 
 		// 输出
 		$this->assign('product',$product);
-		$this->assign('driver',$driver);
-		$this->assign('third',$third);
+		// $this->assign('driver',$driver);
+		// $this->assign('third',$third);
 
 		// 检查是否没有搜索结果
-		if(is_null($product) && is_null($driver) && is_null($third))
+		if(is_null($product) /*&& is_null($driver) && is_null($third)*/)
 		{
 			$this->assign('data_empty', 1);
 		}
