d9825f955925216ad7f5f536cb6737855c455013
MinoWu <minowu@foxmail.com>
2013-05-07 21:59:08 +0800

    关闭admin index的链接和跳转。

--- App/Lib/Admin/Controller/IndexController.php
+++ App/Lib/Admin/Controller/IndexController.php
@@ -2,11 +2,13 @@
 
 use Think\Controller as Controller;
 use Think\Auth as Auth;
+use Think\Redirect as Redirect;
+use Think\Url as Url;
 
 class IndexController extends Controller
 {
 	public function index()
 	{
-		$this->display();
+		Redirect::success('welcome', Url::make('product/index'));
 	}
 }
