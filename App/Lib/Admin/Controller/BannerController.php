875e76a510e865f4c4554fa4f91c23b56fc255a9
MinoWu <minowu@foxmail.com>
2013-05-07 22:19:35 +0800

    重载enable

--- App/Lib/Admin/Controller/BannerController.php
+++ App/Lib/Admin/Controller/BannerController.php
@@ -1,6 +1,7 @@
 <?php
 
 use Smartadmin\Controller\Content as Controller;
+use Think\Redirect as Redirect;
 
 class BannerController extends Controller
 {
@@ -9,4 +10,32 @@ class BannerController extends Controller
 	protected $cover_thumb_name = '1680x445';
 
 	protected $category_id = 5;
+
+	/**
+	 * Update model hidden
+	 * 编辑model的hidden字段
+	 *
+	 * @return void
+	 */
+	public function enable()
+	{
+		if($this->pk_id) {
+
+			$this->model->find($this->pk_id);
+
+			if($this->model->hidden == 0)
+			{
+				$count = $this->model->where(array('hidden' => 1))->count();
+				if($count >= 4)
+				{
+					Redirect::success('仅能公开4张图片');
+				}
+			}
+
+			$this->model->hidden = !$this->model->hidden;
+			$this->model->save();
+
+			Redirect::success('状态发布成功');
+		}
+	}
 }
