6d720454dc669ff8e228c7476771cd2c9f1086eb
MinoWu <minowu@foxmail.com>
2013-06-01 13:03:23 +0800

    添加Lang标签和语言包文件。

---
+++ App/Lang/en-us.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+	// 账号相关
+	'REGISTER' => 'register',
+	'LOGIN' => 'login',
+	'LOGOUT' => 'logout',
+
+	// 导航页面
+	'PRODUCT_SHOW' => 'Product',
+	'PRODUCT_DRIVER' => 'Drivers',
+	'THIRD_DRIVER' => 'Third Drivers',
+	'INFO_DOWNLOAD' => 'Download',
+	'FEED_SUPPORT' => 'Support',
+	'ABOUT_US' => 'About us'
+);
---
+++ App/Lang/zh-cn.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+	// 账号相关
+	'REGISTER' => '注册',
+	'LOGIN' => '登录',
+	'LOGOUT' => '注销 insona 账号',
+
+	// 导航页面
+	'PRODUCT_SHOW' => '产品展示',
+	'PRODUCT_DRIVER' => '产品驱动',
+	'THIRD_DRIVER' => '三方驱动',
+	'INFO_DOWNLOAD' => '资料下载',
+	'FEED_SUPPORT' => '反馈与建议',
+	'ABOUT_US' => '关于我们'
+);
