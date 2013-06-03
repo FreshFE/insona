afaf85023fbf80a4068fb23adaff18b21a5b1d22
Mino Wu <minowu@foxmail.com>
2013-06-03 08:50:24 +0800

    更新翻译。

--- App/Lang/en-us.php
+++ App/Lang/en-us.php
@@ -8,10 +8,65 @@ return array(
 	'LOGOUT' => 'logout',
 
 	// 导航页面
+	'HOME' => 'Home',
 	'PRODUCT_SHOW' => 'Product',
 	'PRODUCT_DRIVER' => 'Drivers',
 	'THIRD_DRIVER' => 'Third Drivers',
 	'INFO_DOWNLOAD' => 'Download',
 	'FEED_SUPPORT' => 'Support',
-	'ABOUT_US' => 'About us'
+	'ABOUT_US' => 'About us',
+
+	// 常用
+	'VIEW_DETAIL' => 'View detail',
+	'VIEW_MORE' => 'More',
+	'CLICK_TO_DOWNLOAD' => 'Donwload',
+
+	// 产品展示
+	'PRODUCT_TOBUY' => 'Click to buy',
+	'PRODUCT_TO_VALID_ACCOUNT' => 'You have registered, but it is not work.',
+	'PRODUCT_TO_LOGIN' => 'logined to see price and link of this product',
+
+
+	// 下载
+	'DOWNLOAD_ROLE_PUBLIC' => 'Anonymous',
+	'DOWNLOAD_ROLE_MEMBER' => 'Member',
+	'DOWNLOAD_JUMPTO' => 'Jump to',
+
+	// 反馈和支持
+	'SUPPORT_TITLE' => 'Title',
+	'SUPOORT_PRODUCT_TYPE' => 'Product',
+	'SUPPORT_CONTENT' => 'Content',
+	'SUPPORT_FONT_NUM' => 'Characters',
+
+	// 注册
+	'SURENAME' => 'Fullname',
+	'PHONE' => 'Phone',
+	'COMPANY' => 'Company',
+	'BIRTHDAY' => 'birthday',
+	'UPLOAD_LICENSE' => 'Company license',
+	'IDCARD' => 'ID Card',
+	'COUNTRY' => 'Country',
+	'CHINA' => 'China',
+	'US' => 'US',
+	'UK' => 'UK',
+	'EMAIL' => 'E-mail',
+	'CITY' => 'City',
+	'PASSWORD' => 'Password',
+	'ADDRESS' => 'Address',
+	'REPASSWORD' => 'Re password',
+	'INSTALL_TYPE' => 'Install type',
+	'HOUSE' => 'House',
+	'LIGHT_COMMERCIAL' => 'Commercial',
+	'ONDOOR_SERVICE' => 'Visiting service',
+	'INTERNET_INSTALL' => 'Network install',
+	'SELF_INSTALL' => 'Self install',
+
+	// 登录
+	'FORGET_PASSWORD' => 'Forget password？',
+
+	// 忘记密码
+	'FORGET_PASSWORD_TIP' => 'Type your registerd E-mail to find your password',
+
+	'PLEASE_NEW_PASSWORD' => 'Please type new password',
+	'PLEASE_REPASSWORD' => 'Please confirm your password'
 );
