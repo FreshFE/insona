283168f329376ce728d644dab4914ad1708294a1
Mino Wu <minowu@foxmail.com>
2013-06-03 08:24:03 +0800

    添加翻译。

--- App/Lang/zh-cn.php
+++ App/Lang/zh-cn.php
@@ -14,5 +14,28 @@ return array(
 	'THIRD_DRIVER' => '三方驱动',
 	'INFO_DOWNLOAD' => '资料下载',
 	'FEED_SUPPORT' => '反馈与建议',
-	'ABOUT_US' => '关于我们'
+	'ABOUT_US' => '关于我们',
+
+	// 常用
+	'VIEW_DETAIL' => '了解详情',
+	'VIEW_MORE' => '查看更多',
+	'CLICK_TO_DOWNLOAD' => '点击下载',
+
+	// 产品展示
+	'PRODUCT_TOBUY' => '点击即可购买',
+	'PRODUCT_TO_VALID_ACCOUNT' => '您已注册，但您的账号还未激活',
+	'PRODUCT_TO_LOGIN' => '登录后即可查看产品的价格和购买链接',
+
+
+	// 下载
+	'DOWNLOAD_ROLE_PUBLIC' => '非会员区',
+	'DOWNLOAD_ROLE_MEMBER' => '会员区',
+	'DOWNLOAD_JUMPTO' => '跳转至',
+
+	// 反馈和支持
+	'SUPPORT_TITLE' => '标题',
+	'SUPOORT_PRODUCT_TYPE' => '产品型号',
+	'SUPPORT_CONTENT' => '内容',
+	'SUPPORT_FONT_NUM' => '个字符内'
+
 );
