f7fbb48d038fd31c5babc84a51ae7e95afe7b668
MinoWu <minowu@foxmail.com>
2013-07-09 15:30:48 +0800

    给search页面添加分页功能和样式

--- App/Lib/Home/Controller/SearchController.php
+++ App/Lib/Home/Controller/SearchController.php
@@ -11,12 +11,15 @@ class SearchController extends Controller
 
 		$keyword = $_GET['q'];
 
+		$page = $_GET['page'] ? $_GET['page'] : 1;
+		$listRow = 5;
+
 		$condition['title'] = array("like", array('%'.$keyword.'%'));
 		$condition['content'] = array("like", array('%'.$keyword.'%'));
 		$condition['_logic'] = 'or';
 
 		// 产品
-		$product = M('Product')->where($condition)->select();
+		$product = M('Product')->where($condition)->page($page, $listRow)->select();
 		// $driver = M('Driver')->where($condition)->select();
 		// $third = M('Third')->where($condition)->select();
 
@@ -24,6 +27,9 @@ class SearchController extends Controller
 		$this->assign('product',$product);
 		// $this->assign('driver',$driver);
 		// $this->assign('third',$third);
+
+		$pager = M('Product')->where($condition)->pager($page, $listRow);
+		$this->assign('pager', $pager);
 
 		// 检查是否没有搜索结果
 		if(is_null($product) /*&& is_null($driver) && is_null($third)*/)
